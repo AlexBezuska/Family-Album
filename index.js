355f1f7146edf48e1e4bed9604d586ec6f60cfdc
Alex Bezuska <abezuska@gmail.com>
2016-08-09 11:22:24 -0400

    add photos displaying on a black page

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
+var photosPath = "photos"
+app.use(express.static('public'));
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+  fs.readdir("public/" + photosPath, function(err, items) {
+    var html ="";
+    for (var i=0; i<items.length; i++) {
+        html += '<img src="http://localhost:3000/' + photosPath + '/' + items[i] + '" style="width: 200px;"/>';
+    }
+    res.send(html);
+  });
+
 });
 
 app.listen(3000, function () {
